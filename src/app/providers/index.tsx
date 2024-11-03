b5b0b5d4d157c84ed1c51ba3f2f90159b0668529
yehor <kravtsov.egor@kronas.com.ua>
2024-11-03 15:05:09 +0100

    Added Chakra UI for making responsive design

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 
 import router from "@/app/config/routing/router.tsx";
 
 function Providers() {
-  return <RouterProvider router={router} />;
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default Providers;
